64afdac2a954f7fc084fbdf248a6f8c08ddd5c9c
pmall <pierre.mallinjoud@gmail.com>
2018-04-27 15:45:23 +0200

    add more method to Session data class

--- spec/Session.spec.php
+++ spec/Session.spec.php
@@ -6,15 +6,158 @@ describe('Session', function () {
 
     beforeEach(function () {
 
-        $this->data = ['key1' => 'value1'];
+        $this->data = [
+            'key1' => 'value1',
+            'key2' => 'value2',
+            Session::FLASH => [
+                'key3' => 'value3',
+            ],
+        ];
 
         $this->session = new Session($this->data);
 
     });
 
+    it('should unset the flash namespace of the data array ref', function () {
+
+        expect($this->data)->not->toContainKey(Session::FLASH);
+
+    });
+
+    describe('->id()', function () {
+
+        it('should return the current session id', function () {
+
+            session_id('session_id');
+
+            $test = $this->session->id();
+
+            expect($test)->toEqual('session_id');
+
+        });
+
+    });
+
+    describe('->regenerate_id()', function () {
+
+        beforeEach(function () {
+
+            $this->test = null;
+
+            allow('session_regenerate_id')->toBeCalled()->andRun(function ($flag) {
+
+                $this->test = $flag;
+
+            });
+
+        });
+
+        context('when called with false as parameter', function () {
+
+            it('should proxy session_regenerate_id() with false as parameter', function () {
+
+                $this->session->regenerate_id();
+
+                expect($this->test)->not->toBeNull();
+                expect($this->test)->toBeFalsy();
+
+            });
+
+        });
+
+        context('when called with true as parameter', function () {
+
+            it('should proxy session_regenerate_id() with true as parameter', function () {
+
+                $this->session->regenerate_id(true);
+
+                expect($this->test)->not->toBeNull();
+                expect($this->test)->toBeTruthy();
+
+            });
+
+        });
+
+    });
+
+    describe('all', function () {
+
+        context('when the flashed data array is empty', function () {
+
+            it('should return all the current and previous data array', function () {
+
+                $test = $this->session->all();
+
+                expect($test)->toEqual([
+                    'key1' => 'value1',
+                    'key2' => 'value2',
+                    'key3' => 'value3',
+                ]);
+
+            });
+
+        });
+
+        context('when the flashed data array is not empty', function () {
+
+            it('should return all the current, flashed and previous data array', function () {
+
+                $this->session->flash('key4', 'value4');
+
+                $test = $this->session->all();
+
+                expect($test)->toEqual([
+                    'key1' => 'value1',
+                    'key2' => 'value2',
+                    'key3' => 'value3',
+                    'key4' => 'value4',
+                ]);
+
+            });
+
+        });
+
+        context('when a key is both in current and previous data array', function () {
+
+            it('should return the current value for this key', function () {
+
+                $this->session->set('key3', 'new_value3');
+
+                $test = $this->session->all();
+
+                expect($test)->toEqual([
+                    'key1' => 'value1',
+                    'key2' => 'value2',
+                    'key3' => 'new_value3',
+                ]);
+
+            });
+
+        });
+
+        context('when a key is both in flashed and previous data array', function () {
+
+            it('should return the flashed value for this key', function () {
+
+                $this->session->flash('key3', 'new_value3');
+
+                $test = $this->session->all();
+
+                expect($test)->toEqual([
+                    'key1' => 'value1',
+                    'key2' => 'value2',
+                    'key3' => 'new_value3',
+                ]);
+
+            });
+
+        });
+
+    });
+
     describe('->has()', function () {
 
-        context('when the given key is present in the data array', function () {
+        context('when the given key is present in the current data array', function () {
 
             it('should return true', function () {
 
@@ -26,11 +169,37 @@ describe('Session', function () {
 
         });
 
-        context('when the given key is not present in the data array', function () {
+        context('when the given key is in the previous data array', function () {
+
+            it('should return true', function () {
+
+                $test = $this->session->has('key3');
+
+                expect($test)->toBeTruthy();
+
+            });
+
+        });
+
+        context('when the given key is in the flashed data array', function () {
+
+            it('should return true', function () {
+
+                $this->session->flash('key4', 'value4');
+
+                $test = $this->session->has('key4');
+
+                expect($test)->toBeTruthy();
+
+            });
+
+        });
+
+        context('when the given key is not present in the current, previous or flashed data array', function () {
 
             it('should return false', function () {
 
-                $test = $this->session->has('key2');
+                $test = $this->session->has('notset');
 
                 expect($test)->toBeFalsy();
 
@@ -42,25 +211,79 @@ describe('Session', function () {
 
     describe('->get()', function () {
 
-        context('when no default value is given', function () {
+        context('when the given key is present in the current data array', function () {
 
-            context('when the given key is associated with a value', function () {
+            it('should return the value associated with the given key', function () {
 
-                it('should return the value associated with the given key', function () {
+                $test = $this->session->get('key1');
 
-                    $test = $this->session->get('key1');
-
-                    expect($test)->toEqual('value1');
-
-                });
+                expect($test)->toEqual('value1');
 
             });
 
-            context('when the given key is not associated with a value', function () {
+        });
+
+        context('when the given key is present in the previous data array', function () {
+
+            it('should return the value associated with the given key', function () {
+
+                $test = $this->session->get('key3');
+
+                expect($test)->toEqual('value3');
+
+            });
+
+        });
+
+        context('when the given key is present in the flashed data array', function () {
+
+            it('should return the value associated with the given key', function () {
+
+                $this->session->flash('key4', 'value4');
+
+                $test = $this->session->get('key4');
+
+                expect($test)->toEqual('value4');
+
+            });
+
+        });
+
+        context('when the given key is present both in the current and previous data array', function () {
+
+            it('should return the current value associated with the given key', function () {
+
+                $this->session->set('key3', 'new_value3');
+
+                $test = $this->session->get('key3');
+
+                expect($test)->toEqual('new_value3');
+
+            });
+
+        });
+
+        context('when the given key is present both in the flashed and previous data array', function () {
+
+            it('should return the flashed value associated with the given key', function () {
+
+                $this->session->flash('key3', 'new_value3');
+
+                $test = $this->session->get('key3');
+
+                expect($test)->toEqual('new_value3');
+
+            });
+
+        });
+
+        context('when the given key is not present in the current, previous or flashed data array', function () {
+
+            context('when a default value is given', function () {
 
                 it('should return the given default value', function () {
 
-                    $test = $this->session->get('key2', 'default');
+                    $test = $this->session->get('notset', 'default');
 
                     expect($test)->toEqual('default');
 
@@ -68,27 +291,11 @@ describe('Session', function () {
 
             });
 
-        });
-
-        context('when a default value is given', function () {
-
-            context('when the given key is associated with a value', function () {
-
-                it('should return the value associated with the given key', function () {
-
-                    $test = $this->session->get('key1', 'default');
-
-                    expect($test)->toEqual('value1');
-
-                });
-
-            });
-
-            context('when the given key is not associated with a value', function () {
+            context('when no default value is given', function () {
 
                 it('should return null', function () {
 
-                    $test = $this->session->get('key2');
+                    $test = $this->session->get('notset');
 
                     expect($test)->toBeNull();
 
@@ -104,21 +311,65 @@ describe('Session', function () {
 
         it('should associate the given key with the given value', function () {
 
-            $this->session->set('key2', 'value2');
+            $this->session->set('key4', 'value4');
 
-            $test = $this->session->get('key2');
+            $test = $this->session->get('key4');
 
-            expect($test)->toEqual('value2');
+            expect($test)->toEqual('value4');
 
         });
 
         it('should update the data array ref', function () {
 
-            $this->session->set('key2', 'value2');
+            $this->session->set('key4', 'value4');
 
-            $test = $this->data['key2'];
+            $test = $this->data['key4'];
 
-            expect($test)->toEqual('value2');
+            expect($test)->toEqual('value4');
+
+        });
+
+        it('should overvrite keys with the same name in the flashed data array', function () {
+
+            $this->session->flash('key4', 'flashed');
+            $this->session->set('key4', 'value4');
+
+            expect($this->data['key4'])->toEqual('value4');
+            expect($this->data[Session::FLASH])->not->toContainKey('key4');
+
+        });
+
+    });
+
+    describe('->flash()', function () {
+
+        it('should associate the given key with the given value', function () {
+
+            $this->session->flash('key4', 'value4');
+
+            $test = $this->session->get('key4');
+
+            expect($test)->toEqual('value4');
+
+        });
+
+        it('should update the data array ref', function () {
+
+            $this->session->flash('key4', 'value4');
+
+            $test = $this->data[Session::FLASH]['key4'];
+
+            expect($test)->toEqual('value4');
+
+        });
+
+        it('should overvrite keys with the same name in the current data array', function () {
+
+            $this->session->set('key4', 'current');
+            $this->session->flash('key4', 'value4');
+
+            expect($this->data[Session::FLASH]['key4'])->toEqual('value4');
+            expect($this->data)->not->toContainKey('key4');
 
         });
 
@@ -126,21 +377,79 @@ describe('Session', function () {
 
     describe('->unset()', function () {
 
-        it('should unset the given key', function () {
+        context('when the key is in the current data array', function () {
 
-            $this->session->unset('key1');
+            it('should unset the given key', function () {
 
-            $test = $this->session->has('key1');
+                $this->session->unset('key1');
 
-            expect($test)->toBeFalsy();
+                $test = $this->session->has('key1');
+
+                expect($test)->toBeFalsy();
+
+            });
+
+            it('should update the data array ref', function () {
+
+                $this->session->unset('key1');
+
+                expect($this->data)->not->toContainKey('key1');
+
+            });
+
+        });
+
+        context('when the key is in the previous data array', function () {
+
+            it('should unset the given key', function () {
+
+                $this->session->unset('key3');
+
+                $test = $this->session->has('key3');
+
+                expect($test)->toBeFalsy();
+
+            });
+
+        });
+
+        context('when the key is in the flashed data array', function () {
+
+            it('should unset the given key', function () {
+
+                $this->session->flash('key4', 'session4');
+
+                $this->session->unset('key4');
+
+                $test = $this->session->has('key4');
+
+                expect($test)->toBeFalsy();
+
+            });
+
+        });
+
+    });
+
+    describe('->delete()', function () {
+
+        it('should unset all the keys', function () {
+
+            $this->session->flash('key4', 'value4');
+
+            $this->session->delete();
+
+            $test = $this->session->all();
+
+            expect($this->data)->toEqual([]);
 
         });
 
         it('should update the data array ref', function () {
 
-            $this->session->unset('key1');
+            $this->session->delete();
 
-            expect($this->data)->not->toContainKey('key1');
+            expect($this->data)->toEqual([]);
 
         });
 
--- src/Session.php
+++ src/Session.php
@@ -5,11 +5,25 @@ namespace Ellipse;
 class Session
 {
     /**
+     * The flash namespace.
+     *
+     * @var string
+     */
+    const FLASH = '::flash::';
+
+    /**
      * The reference to an array of session data.
      *
      * @var array
      */
     private $data;
+
+    /**
+     * The flashed data from the previous session.
+     *
+     * @var array
+     */
+    private $flash;
 
     /**
      * Set up a session with the given ref to an array of session data.
@@ -19,6 +33,47 @@ class Session
     public function __construct(array &$data)
     {
         $this->data = &$data;
+
+        $this->flash = $this->data[self::FLASH] ?? [];
+
+        unset($this->data[self::FLASH]);
+    }
+
+    /**
+     * Return the session id.
+     *
+     * @return string
+     */
+    public function id(): string
+    {
+        return session_id();
+    }
+
+    /**
+     * Regenerate the session id.
+     *
+     * @param bool $delete_old_session
+     * @return void
+     */
+    public function regenerate_id(bool $delete_old_session = false)
+    {
+        return session_regenerate_id($delete_old_session);
+    }
+
+    /**
+     * Return all the session data.
+     *
+     * @return array
+     */
+    public function all(): array
+    {
+        $data = array_merge($this->data, $this->data[self::FLASH] ?? []);
+
+        unset($data[self::FLASH]);
+
+        $previous = array_diff_key($this->flash, $data);
+
+        return array_merge($data, $previous);
     }
 
     /**
@@ -29,7 +84,9 @@ class Session
      */
     public function has(string $key): bool
     {
-        return isset($this->data[$key]);
+        return isset($this->flash[$key])
+            || isset($this->data[$key])
+            || isset($this->data[self::FLASH][$key]);
     }
 
     /**
@@ -42,7 +99,10 @@ class Session
      */
     public function get(string $key, $default = null)
     {
-        return $this->has($key) ? $this->data[$key] : $default;
+        return $this->data[$key]
+            ?? $this->data[self::FLASH][$key]
+            ?? $this->flash[$key]
+            ?? $default;
     }
 
     /**
@@ -54,7 +114,23 @@ class Session
      */
     public function set(string $key, $value)
     {
+        unset($this->data[self::FLASH][$key]);
+
         $this->data[$key] = $value;
+    }
+
+    /**
+     * Associate the given key with the given value within the flash namespace.
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
+    public function flash(string $key, $value)
+    {
+        unset($this->data[$key]);
+
+        $this->data[self::FLASH][$key] = $value;
     }
 
     /**
@@ -65,6 +141,28 @@ class Session
      */
     public function unset(string $key)
     {
+        unset($this->flash[$key]);
         unset($this->data[$key]);
+        unset($this->data[self::FLASH][$key]);
+    }
+
+    /**
+     * Unset all keys.
+     *
+     * @return void
+     */
+    public function delete()
+    {
+        foreach (array_keys($this->data) as $key) {
+
+            unset($this->data[$key]);
+
+        }
+
+        foreach (array_keys($this->flash) as $key) {
+
+            unset($this->flash[$key]);
+
+        }
     }
 }
